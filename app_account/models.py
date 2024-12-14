daccbd61b50ce1255873aa4f3e11ea93fc4dc651
SergeyK1983 <ssp-serg@yandex.ru>
2024-12-14 21:32:29 +0300

    feat(2024.12.14) added models AssignedJWTAccessToken, AssignedJWTRefreshToken

--- app_account/models.py
+++ app_account/models.py
@@ -1,7 +1,8 @@
 import uuid
 from datetime import datetime
-from sqlalchemy import String, UUID, Boolean, DateTime, func
-from sqlalchemy.orm import Mapped, mapped_column
+
+from sqlalchemy import BigInteger, String, UUID, Boolean, DateTime, func, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.database import Base
 
@@ -26,10 +27,59 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True, index=True)
     first_name: Mapped[str] = mapped_column(String(100), nullable=True, comment="Имя пользователя")
     last_name: Mapped[str] = mapped_column(String(100), nullable=True, comment="Фамилия пользователя")
-    is_superuser: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, comment="супер пользователь")
+    is_superuser: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, comment="Суперпользователь")
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True, comment="Активен")
-    is_staff: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, comment="супер пользователь")
+    is_staff: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, comment="Персонал")
+
+    access_tokens: Mapped[list["AssignedJWTAccessToken"]] = relationship(
+        back_populates="user", lazy="joined", cascade="all, delete-orphan"
+    )
+    refresh_tokens: Mapped[list["AssignedJWTRefreshToken"]] = relationship(
+        back_populates="user", lazy="joined", cascade="all, delete-orphan"
+    )
 
     def __repr__(self):
         return f"{self.id}-{self.username}"
+
+
+class AssignedJWTAccessToken(Base):
+    """
+    Назначенный токен доступа
+    """
+    __tablename__ = "assigned_jwt_access_token"
+
+    id: Mapped[BigInteger] = mapped_column(BigInteger, unique=True, primary_key=True)
+    jti: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), nullable=False, unique=True, index=True, comment="Идентификатор токена"
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, comment="Активен")
+    expired_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, comment="Окончание доступа")
+    device_id: Mapped[str] = mapped_column(String(100), default="Не указано", comment="Устройство пользователя")
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
+    user: Mapped[User] = relationship(back_populates="access_tokens", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"{self.id}-{self.is_active}-{self.expired_time}"
+
+
+class AssignedJWTRefreshToken(Base):
+    """
+    Назначенный токен обновления
+    """
+    __tablename__ = "assigned_jwt_refresh_token"
+
+    id: Mapped[BigInteger] = mapped_column(BigInteger, unique=True, primary_key=True)
+    jti: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), nullable=False, unique=True, index=True, comment="Идентификатор токена"
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, comment="Активен")
+    expired_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, comment="Окончание доступа")
+    device_id: Mapped[str] = mapped_column(String(100), default="Не указано", comment="Устройство пользователя")
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
+    user: Mapped[User] = relationship(back_populates="refresh_tokens", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"{self.id}-{self.is_active}-{self.expired_time}"
 
